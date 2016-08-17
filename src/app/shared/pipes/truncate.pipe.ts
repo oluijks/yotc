67592493a81dca5d5c494ca852bf4289fdd2ba6e
Olaf Luijks <oluijks@gmail.com>
2016-08-17 22:25:20 +0200

    Some layout improvements

--- src/app/shared/pipes/truncate.pipe.ts
+++ src/app/shared/pipes/truncate.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
   transform(value: string, args: string[]): string {
-    let limit = args.length > 0 ? parseInt(args[0], 25) : 25;
+    let limit = args.length > 0 ? parseInt(args[0], 15) : 15;
     let trail = args.length > 1 ? args[1] : '...';
 
     return value.length > limit ? value.substring(0, limit) + trail : value;
